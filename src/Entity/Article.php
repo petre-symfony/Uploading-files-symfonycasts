b24e052ed1b48e574cd3ef6884b0120a44cb1e4b
petrero <petrero@gmail.com>
2019-11-30 13:49:21 +0200

    19.1.Setup for Uploading Private Article References
    New challenge folks! Our alien authors are begging for a new feature: they want to be able to upload "supporting" files and attach them to the article - like PDFs that they're referencing, images... text notes... really anything. But these files will only be visible to anyone that can edit an article. I'll call these "article references" and every article will be able to have zero to many references, which is where things start to get interesting.
    
    Creating the ArticleReference Entity
    
    Let's create the new entity:
    php bin/console make:entity
    
    Call it ArticleReference and give it an article property. This will be a relation back to the Article class. This will be a ManyToOne relation: each Article can have many ArticleReferences. Then, this will be not null in the database: every ArticleReference must be related to an Article. Say yes to map the other side of the relationship - it's convenient to be able to say $article->getArticleReferences(). And no to orphan removal - we won't be using that feature.
    
    Nice! Ok, this needs a few more fields: filename a string that will hold the filename on the filesystem, originalFilename, a string that will hold the original filename that was on the user's system - more on that later - and mimeType - we'll use that to store what type of file it is - which will come in handy later.

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -85,9 +85,15 @@ class Article {
 	 */
 	private $specificLocationName;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\ArticleReference", mappedBy="article")
+     */
+    private $articleReferences;
+
 	public function __construct() {
 		$this->comments = new ArrayCollection();
 		$this->tags = new ArrayCollection();
+        $this->articleReferences = new ArrayCollection();
 	}
 
 	public function getId() {
@@ -273,4 +279,30 @@ class Article {
 
 		return $this;
 	}
+
+    /**
+     * @return Collection|ArticleReference[]
+     */
+    public function getArticleReferences(): Collection {
+        return $this->articleReferences;
+    }
+    
+    public function addArticleReference(ArticleReference $articleReference): self {
+        if (!$this->articleReferences->contains($articleReference)) {
+            $this->articleReferences[] = $articleReference;
+            $articleReference->setArticle($this);
+        }
+        return $this;
+    }
+
+    public function removeArticleReference(ArticleReference $articleReference): self {
+        if ($this->articleReferences->contains($articleReference)) {
+            $this->articleReferences->removeElement($articleReference);
+            // set the owning side to null (unless already changed)
+            if ($articleReference->getArticle() === $this) {
+                $articleReference->setArticle(null);
+            }
+        }
+        return $this;
+    }
 }
