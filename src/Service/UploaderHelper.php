13d9b8280c777e40f604a1a0578c5eb0f10280e6
petrero <petrero@gmail.com>
2019-11-30 18:21:36 +0200

    23.2.Endpoint for Downloading Private Files{Creating a Read File Stream}
    Creating a Read File Stream
    
    In some ways, our job in the controller is really simple: read the contents of the file and send it to the user. But... we don't actually want to read the contents of the file into a string and then put it in a Response. Because if it's a large file, that will eat up PHP memory.
    
    This is already why, in UploaderHelper, we're using a stream to write the file. And now, we'll use a stream to read it. To keep all this streaming logic centralized in this class, add a new public function readStream() with a string $path argument and bool $isPublic so we know which of these two filesystems to read from.
    
    Above the method, advertise that this will return a resource - PHP doesn't have a resource return type yet. Inside, step 1 is to get the right filesystem using the $isPublic argument.
    
    Then, $resource = $filesystem->readStream($path).
    
    That's... pretty much it! But hold Cmd or Ctrl and click to see the readStream() method. Ah yes, if this fails, Flysystem will return false. So let's code defensively: if ($resource === false), throw a new \Exception() with a nice message:
    
      Error opening stream for %s
    
    and pass $path. At the bottom, return $resource.
    
    This is great! We now have an easy way to get a stream to read any file in our filesystems... which will work if the file is stored locally or somewhere else.

--- src/Service/UploaderHelper.php
+++ src/Service/UploaderHelper.php
@@ -88,6 +88,24 @@ class UploaderHelper {
 				->getBasePath() . $this->publicAssetBaseUrl .'/'. $path;
 	}
 
+    /**
+     * @return resource
+     */
+    public function readStream(string $path, bool $isPublic){
+        $filesystem = $isPublic ? $this->filesystem : $this->privateFilesystem;
+
+        $resource = $filesystem->readStream($path);
+
+        if ($resource === false){
+            throw new \Exception(sprintf(
+                'Error opening stream for "%s"',
+                $path
+            ));
+        }
+
+        return $resource;
+    }
+
     private function uploadFile(
         File $file,
         string $directory,
