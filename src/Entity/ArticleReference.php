076520e6fa797a32b20d3bf58299ac6300e0b718
petrero <petrero@gmail.com>
2019-12-01 10:16:12 +0200

    31.1.Reordering the Files
    What else do you want to add to our file gallery widget? How about allowing them to be reordered? Yea, that isn't really related to uploading either, but a lot of people asked for it... so, let's do it!
    
    Adding the position Field
    
    To start, the ArticleReference entity needs a field that can store its order in the list. Find your terminal and run:
    php bin/console make:entity
    
    Update ArticleReference and add one new field position. This is an integer and make it not nullable. Cool!
    
    Go find the property position, there it is. Make it default to 0: until the user decides to reorder stuff, setting them all to 0 is fine.

--- src/Entity/ArticleReference.php
+++ src/Entity/ArticleReference.php
@@ -45,6 +45,11 @@ class ArticleReference {
    */
   private $mimeType;
 
+  /**
+ * @ORM\Column(type="integer")
+ */
+  private $position = 0;
+
   public function __construct(Article $article) {
     $this->article = $article;
   }
@@ -89,5 +94,14 @@ class ArticleReference {
 
   public function getFilePath():string {
     return UploaderHelper::ARTICLE_REFERENCE.'/'.$this->getFilename();
+  }
+
+  public function getPosition(): ?int {
+    return $this->position;
+  }
+
+  public function setPosition(int $position): self {
+    $this->position = $position;
+    return $this;
   }
 }
